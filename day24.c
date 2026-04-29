43bdfda1b457aa6c3b190c3719c064b974cf5160
kathait-maker <kathaitaaditya37@gmail.com>
2026-04-29 14:21:20 +0530

    day24.c

---
+++ day24.c
@@ -0,0 +1,80 @@
+/*Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer key
+
+Output:
+- Print the linked list elements after deletion, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+30
+
+Output:
+10 20 40 50
+
+Explanation:
+Traverse list, find first node with key, remove it by adjusting previous node's next pointer.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, key;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Create linked list
+    for(int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Input key
+    scanf("%d", &key);
+
+    struct Node *curr = head, *prev = NULL;
+
+    // Delete first occurrence
+    while(curr != NULL) {
+        if(curr->data == key) {
+            if(prev == NULL) {   // If key is in first node
+                head = curr->next;
+            } else {
+                prev->next = curr->next;
+            }
+            free(curr);
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // Print updated list
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
